f4b81cea6989d0333507f26f4d9486c7626927ce
Maxawa <sanula.de.silva@gmail.com>
2025-05-31 14:57:04 +0200

    mapAlgorithm now reads the costmap

--- Assets/MapAlgorithm.cs
+++ Assets/MapAlgorithm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1815082cdf450dd1592f5debdb92288caaade385b75b15113386ee3dae0408c4
-size 2574
+oid sha256:7dbfc7172e694be2b5ab42a5b5e0da77e186589c1776161170c08da5d22762b0
+size 2594
