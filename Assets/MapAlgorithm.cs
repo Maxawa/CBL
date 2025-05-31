112b3e8bbe70a93cb0bf1554d0ec128f30685090
Maxawa <sanula.de.silva@gmail.com>
2025-05-31 18:23:02 +0200

    Added automatic costmap saving and loading

--- Assets/MapAlgorithm.cs
+++ Assets/MapAlgorithm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dbfc7172e694be2b5ab42a5b5e0da77e186589c1776161170c08da5d22762b0
-size 2594
+oid sha256:9a594768bd060ff721e00619640cc7c1b270d2067c799d49ed61e3eb95dbaa44
+size 3621
