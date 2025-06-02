de16307ae86df9b1494452e336f9ae9f297fa031
Maxawa <sanula.de.silva@gmail.com>
2025-06-02 14:55:33 +0200

    a convolution is used to help make the difference texture

--- Assets/MapAlgorithm.cs
+++ Assets/MapAlgorithm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bb6ba5243a526c2c58d2eb12da84bbb0c6db412624b0cb12a6aeb56fc749c93
-size 4361
+oid sha256:350c41b7cc73180a000cef78408bf580d82d5406f7c3cf7936de4b8522dd00ce
+size 4623
