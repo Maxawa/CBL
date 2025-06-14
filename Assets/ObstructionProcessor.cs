a8257eb23e2376da0c30e4c3c8d29e408eaf7a0a
Maxawa <sanula.de.silva@gmail.com>
2025-06-14 15:22:43 +0200

    bugfix

--- Assets/ObstructionProcessor.cs
+++ Assets/ObstructionProcessor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96462ce545bd6198f7f4dd3a61c3a6002f16cf0d66bb211937cb70d2fd4b01bd
-size 11457
+oid sha256:8e5c7e0c191b08a7b11de660343090c8a1914cf447ad917596bd85a44d8aba4b
+size 11454
