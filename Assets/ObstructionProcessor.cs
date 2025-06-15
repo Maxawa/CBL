ac0c3bf86a2a9e8b2d6973863b81522140731e2d
Maxawa <sanula.de.silva@gmail.com>
2025-06-15 02:48:13 +0200

    Added a method that converts from unity coordinates to pixel coordinates

--- Assets/ObstructionProcessor.cs
+++ Assets/ObstructionProcessor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e5c7e0c191b08a7b11de660343090c8a1914cf447ad917596bd85a44d8aba4b
-size 11454
+oid sha256:9eaebd92998101c9df42a5089cf844de0cbcf22d35c944d62da752b95cdfd1cc
+size 1041
